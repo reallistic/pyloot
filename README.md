bb831191cbac489d6ac1dc846158dc60dff6c7e9
Michael Chase <3686226+reallistic@users.noreply.github.com>
2020-06-08 14:57:07 -0700

    inital commit with scaffolding

---
+++ README.md
@@ -0,0 +1,8 @@
+PyLoot
+===========
+
+PyLoot is a memory leak detector based on [Dozer](https://github.com/mgedmin/dozer) and [vprof](https://github.com/nvdv/vprof) with added support for process based server workloads.
+
+
+This project is in active development and may contain bugs or otherwise work in ways not expected or intended.
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name="pyloot",
+    versioning="distance",
+    setup_requires="setupmeta",
+    author="Michael Chase",
+    description="Multiprocessing compatible memory leak debugger inspired by dozer/dowser",
+    keywords="memory profiler multiprocessing wsgi asgi",
+    url="https://github.com/reallistic/pyloot",
+    extras_require={"test": ["pytest"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
